9aab710c2b4da33ef5cd28e29a17a52110e9555e
fajarr02 <fajar170720@gmail.com>
2024-05-12 14:26:43 +0700

    Membuat item dapat diklik dan Membuat tombol FAB

--- app/src/main/java/org/d3if3141/asessment2mobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3141/asessment2mobpro/ui/screen/MainScreen.kt
@@ -1,25 +1,51 @@
 package org.d3if3141.asessment2mobpro.ui.screen
 
 import android.content.res.Configuration
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.d3if3141.asessment2mobpro.R
+import org.d3if3141.asessment2mobpro.model.Peminjaman
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    val context = LocalContext.current
+
     Scaffold (
         topBar = {
             TopAppBar(title = {
@@ -28,7 +54,21 @@ fun MainScreen() {
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
-                ))
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    Toast.makeText(context, R.string.tambah_eror, Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.tambah_peminjaman),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
         }
     ){ padding ->
         ScreenContent(modifier =Modifier.padding(padding))
@@ -37,15 +77,81 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
+
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+
+    // val data = emptyList<Peminjaman>
+    val context = LocalContext.current
+
+    if (data.isEmpty()) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            val imagePainter: Painter = painterResource(id = R.drawable.gambar)
+            Image(
+                painter = imagePainter,
+                contentDescription = "gambar uang",
+                modifier = Modifier
+                    .size(width = 150.dp, height = 150.dp) // Set the desired width and height
+                    .padding(8.dp),
+                contentScale = ContentScale.Fit
+            )
+
+            Text(text = stringResource(id = R.string.data_kosong))
+        }
+    } else {
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize(),
+            contentPadding = PaddingValues(bottom = 84.dp)
+        ) {
+            items(data) {
+                ListItem(peminjaman = it) {
+                    val pesan = context.getString(R.string.x_klik, it.nama)
+                    Toast.makeText(context, pesan, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ListItem(peminjaman: Peminjaman, onClick: () ->Unit) {
     Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
+        Text(
+            text = peminjaman.nama,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = peminjaman.jumlah,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+        Text(
+            text = peminjaman.tenggat,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
 
-
+        Text(text = peminjaman.tanggal)
     }
 
+    Divider()
 }
 
 @Preview(showBackground = true)
